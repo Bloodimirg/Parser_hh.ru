9dde25bdd311f8517fb6700cd712c039b988a42d
Bloodimirg <bloodimirg@gmail.com>
2024-05-21 14:46:57 +0300

    add class work with API hh.ru

--- src/hh_parser.py
+++ src/hh_parser.py
@@ -2,13 +2,33 @@ from abc import ABC, abstractmethod
 import requests
 
 
-class JobApi(ABC):
-    pass
+class Parser(ABC):
+
+    def __init__(self):
+        pass
+
+    def connect(self):
+        pass
+
+    def get_vacancies(self):
+        pass
 
 
-class Parser(JobApi):
-    pass
+class HeadHunterAPI(Parser):
+    """Класс для работы с API HeadHunter"""
 
+    def __init__(self):
+        super().__init__()
+        self.url = "https://api.hh.ru/vacancies"
+        self.headers = {'User-Agent': 'HH-User-Agent'}
+        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.vacancies = []
 
-class HH(Parser):
-    pass
+    def load_vacancies(self, search_query):
+        self.params['text'] = search_query
+        while self.params.get('page') != 20:
+            response = requests.get(self.url, headers=self.headers, params=self.params)
+            vacancies = response.json()['items']
+            self.vacancies.extend(vacancies)
+            self.params['page'] += 1
+        return self.vacancies
