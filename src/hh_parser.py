a7d06df423d88c20c309476ca95976a43b5efafe
Bloodimirg <bloodimirg@gmail.com>
2024-05-30 10:43:11 +0300

    метод работы с АПИ разделен на две части

--- src/hh_parser.py
+++ src/hh_parser.py
@@ -3,32 +3,37 @@ import requests
 
 
 class Parser(ABC):
-
+    """Абстрактный класс работы с API"""
+    @abstractmethod
     def __init__(self):
         pass
 
-    def connect(self):
+    @abstractmethod
+    def _connect(self):
         pass
 
-    def get_vacancies(self):
+    @abstractmethod
+    def load_vacancies(self, search_query):
         pass
 
 
 class HeadHunterAPI(Parser):
     """Класс для работы с API HeadHunter"""
-
     def __init__(self):
-        super().__init__()
-        self.url = "https://api.hh.ru/vacancies"
-        self.headers = {'User-Agent': 'HH-User-Agent'}
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
-        self.vacancies = []
+        self._url = "https://api.hh.ru/vacancies"
+        self._headers = {'User-Agent': 'HH-User-Agent'}
+        self._params = {'text': '', 'page': 0, 'per_page': 100}
+        self._vacancies = []
+
+    def _connect(self):
+        response = requests.get(self._url, headers=self._headers, params=self._params)
+        returned_response = response.json()
+        return returned_response
 
     def load_vacancies(self, search_query):
-        self.params['text'] = search_query
-        while self.params.get('page') != 20:
-            response = requests.get(self.url, headers=self.headers, params=self.params)
-            vacancies = response.json()['items']
-            self.vacancies.extend(vacancies)
-            self.params['page'] += 1
-        return self.vacancies
+        self._params['text'] = search_query
+        while self._params.get('page') != 5:
+            vacancies = self._connect()['items']
+            self._vacancies.extend(vacancies)
+            self._params['page'] += 1
+        return self._vacancies
