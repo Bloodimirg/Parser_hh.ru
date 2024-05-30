8669f0210ebbc3b4f8cc7011b4ef5e4a1385b075
Bloodimirg <bloodimirg@gmail.com>
2024-05-30 10:45:48 +0300

    Изменен метод to_json. Дополнены методы валидации, сортировки, дандер_методы

--- src/vacancy.py
+++ src/vacancy.py
@@ -1,6 +1,6 @@
 class Vacancy:
 
-    def __init__(self, country, name, salary, url, requirement, responsibility):
+    def __init__(self, country: str, name: str, salary: int, url: str, requirement: str, responsibility: str):
         self.country = country
         self.name = name
         self.salary = salary
@@ -13,16 +13,18 @@ class Vacancy:
         """Создаем объект"""
         returned_list = []
         for vacancy in vacancies:
+            country = vacancy["area"]["name"]
+            name = vacancy["name"]
             if vacancy['salary']:
-                country = vacancy["area"]["name"]
-                name = vacancy["name"]
-                salary = vacancy.get("salary", {}).get('from', '')
-                url = vacancy["alternate_url"]
-                requirement = vacancy.get('snippet', {}).get('requirement')
-                responsibility = vacancy.get('snippet', {}).get('responsibility')
+                salary = Vacancy.validate_int(vacancy.get("salary", {}).get('from', ''))
+            else:
+                salary = 0
+            url = vacancy["alternate_url"]
+            requirement = Vacancy.validate_str(vacancy.get('snippet').get('requirement'))
+            responsibility = Vacancy.validate_str(vacancy.get('snippet').get('responsibility'))
 
-                vacancy_obj = cls(country, name, salary, url, requirement, responsibility)
-                returned_list.append(vacancy_obj)
+            vacancy_obj = cls(country, name, salary, url, requirement, responsibility)
+            returned_list.append(vacancy_obj)
         return returned_list
 
     @staticmethod
@@ -34,35 +36,83 @@ class Vacancy:
         return filtered_vacancies
 
     @staticmethod
+    def validate_int(value):
+        """Валидация по int"""
+        if value:
+            return value
+        return 0
+
+    @staticmethod
+    def validate_str(word):
+        """Валидация по str"""
+        if word:
+            return word
+        return "Не указано"
+
+    @staticmethod
     def find_by_salary(vacancies, min_salary):
         """Фильтр по зарплате"""
         filtered_vacancies_twoo = [vacancy for vacancy in vacancies if
-                                   vacancy.salary and vacancy.salary >= min_salary]
+                                   vacancy >= min_salary]
         return filtered_vacancies_twoo
 
     @staticmethod
+    def sorted_by_salary(vacancies):
+        """Соритировка по зарплате"""
+        return sorted(vacancies, reverse=True)
+
+    @staticmethod
     def get_top_vacancies(count_vacancies, top_n):
-        """Количество вакансий"""
+        """Количество вакансий для вывода"""
         return count_vacancies[:top_n]
+
+    def __eq__(self, other):
+        if type(other) is type(self):
+            return self.salary == other.salary
+        elif type(other) is not type(self):
+            return self.salary == other
+
+    def __ne__(self, other):
+        if type(other) is type(self):
+            return self.salary != other.salary
+        elif type(other) is not type(self):
+            return self.salary != other
+
+    def __lt__(self, other):
+        if type(other) is type(self):
+            return self.salary < other.salary
+        elif type(other) is not type(self):
+            return self.salary < other
+
+    def __le__(self, other):
+        if type(other) is type(self):
+            return self.salary <= other.salary
+        elif type(other) is not type(self):
+            return self.salary <= other
+
+    def __gt__(self, other):
+        if type(other) is type(self):
+            return self.salary > other.salary
+        elif type(other) is not type(self):
+            return self.salary > other
+
+    def __ge__(self, other):
+        if type(other) is type(self):
+            return self.salary >= other.salary
+        elif type(other) is not type(self):
+            return self.salary >= other
 
     def __str__(self):
         """Вывод на экран"""
         return (f'\n{self.name}'
-                f'\nГород: {self.country or "не указан"}'
-                f'\nЗарплата от: {self.salary or "не указана"}'
-                f'\nСайт: {self.url or "не указан"}'
-                f'\nТребования: {self.requirement or "не указаны"}'
-                f'\nОбязанности: {self.responsible or "не указаны"}')
+                f'\nГород: {self.country}'
+                f'\nЗарплата от: {self.salary or "Не указана"}'
+                f'\nСайт: {self.url}'
+                f'\nТребования: {self.requirement.replace("<highlighttext>", "").replace("</highlighttext>", "")}'
+                f'\nОбязанности: {self.responsible.replace("<highlighttext>", "").replace("</highlighttext>", "")}')
 
     def to_json(self):
         """Преобразование в словарь для JSON"""
-        return {
-            "country": self.country,
-            "name": self.name,
-            "salary": self.salary,
-            "url": self.url,
-            "requirement": self.requirement,
-            "responsibility": self.responsible
-        }
 
+        return self.__dict__
 
